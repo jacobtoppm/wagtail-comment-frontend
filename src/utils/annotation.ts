abf76ad4fc3dffaaee326e492bed97106f003cae
jacobtoppm <jacobtm@torchbox.com>
2021-03-02 13:37:05 +0000

    Update annotation interface

--- src/utils/annotation.ts
+++ src/utils/annotation.ts
@@ -1,10 +1,3 @@
 export interface Annotation {
-  position: string;
-  onDelete(): void;
-  onFocus(): void;
-  onUnfocus(): void;
-  show(): void;
-  hide(): void;
-  setOnClickHandler: (handler: any) => void;
   getDesiredPosition(): number;
 }
